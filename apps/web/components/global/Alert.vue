5e12887020b726a976e56fc901998681c1da319a
BlakeBr0 <luasmrks@gmail.com>
2023-09-11 19:57:11 -0500

    Basic alert component for use in docs/wiki content pages

---
+++ apps/web/components/global/Alert.vue
@@ -0,0 +1,16 @@
+<template>
+  <UAlert color="primary" variant="subtle">
+    <template #title>
+      <strong class="text-lg">{{ title || "Heads Up!" }}</strong>
+    </template>
+    <template #description>
+      <slot></slot>
+    </template>
+  </UAlert>
+</template>
+
+<script setup>
+defineProps({
+  title: String
+});
+</script>
