3538bbfc99487fe69f85e3bb634561891a9475ca
BlakeBr0 <luasmrks@gmail.com>
2024-01-03 23:23:31 -0600

    Remove mx-auto class from ad

--- apps/web/components/Ad.vue
+++ apps/web/components/Ad.vue
@@ -2,7 +2,7 @@
   <adsbygoogle
     class="adsbygoogle text-center"
     :class="{
-      'mx-auto border border-gray-200 dark:border-gray-800 rounded': !adLayout
+      'border border-gray-200 dark:border-gray-800 rounded': !adLayout
     }"
     :ad-layout="adLayout"
     :ad-slot="adSlot"
