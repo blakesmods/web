0c8b6380d84cb75a5748d48f23ddaebc8ccfda0e
BlakeBr0 <luasmrks@gmail.com>
2025-07-27 14:35:09 -0500

    Initial work on Mystical Automation pages

---
+++ apps/web/content/wiki/1.21/mysticalautomation/blocks/fertilizer.md
@@ -0,0 +1,25 @@
+---
+id: fertilizer
+title: Fertilizer
+icon: /assets/mysticalautomation/v1/blocks/fertilizer.png
+sort: 4
+version: 1.0.0
+---
+
+## Overview
+
+The Furnace is an upgraded version of the regular Furnace that can be upgraded for additional speed and efficiency.
+
+## Usage
+
+Furnaces require energy to function. You can either provide energy via any Forge Energy compatible energy transfer, or use the internal solid-fuel based generator.
+
+## Screen Overview
+
+![](/assets/mysticalagriculture/v8/screens/furnace_screen.png)
+
+1. **Energy Bar:** Displays the amount of energy this block contains.
+2. **Fuel Slot:** Burnable items can be placed here to generate energy.
+3. **Input Slot:** Place your input ingredients here.
+4. **Output Slot:** The recipe output will appear here.
+5. **Upgrade Slot:** Place a [Machine Upgrade](../items/machine-upgrades.md) here to upgrade the machine.
---
+++ apps/web/pages/mystical-automation/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <ModPage :mod="mod">
+    <div class="container relative flex flex-col my-8 gap-8 z-10">
+      <KeyFeatures
+        title="Key Features"
+        subtitle="Resource Collection Automated"
+        :features="features"
+      />
+    </div>
+  </ModPage>
+</template>
+
+<script setup>
+import ModPage from "~/components/pages/ModPage.vue";
+import KeyFeatures from "~/components/mods/KeyFeatures.vue";
+
+definePageMeta({
+  layout: "mods"
+});
+
+const mod = getMod("mysticalautomation");
+
+const features = ref([
+  {
+    title: "Infuser",
+    description: [
+      "Automatically infuse Essences together and select a tier to output."
+    ],
+    images: ["/assets/mysticalautomation/v1/blocks/infuser.png"],
+    link: "/wiki/mysticalautomation/blocks/infuser"
+  },
+  {
+    title: "Crafter",
+    description: ["Automatically craft any crafting recipe using the Crafter."],
+    images: ["/assets/mysticalautomation/v1/blocks/crafter.png"],
+    link: "/wiki/mysticalautomation/blocks/crafter"
+  },
+  {
+    title: "Farmer",
+    description: ["Automatically plant, grow and harvest crops."],
+    images: ["/assets/mysticalautomation/v1/blocks/farmer.png"],
+    link: "/wiki/mysticalautomation/blocks/farmer"
+  },
+  {
+    title: "Fertilizer",
+    description: [
+      "Automatically fertilize plants with fertilizers in an area."
+    ],
+    images: ["/assets/mysticalautomation/v1/blocks/fertilizer.png"],
+    link: "/wiki/mysticalautomation/blocks/fertilizer"
+  },
+  {
+    title: "Altarnators",
+    description: [
+      "Automatically craft Infusion and Awakening recipes with their respective Altarnator."
+    ],
+    images: ["/assets/mysticalautomation/v1/blocks/infusion_altarnator.png"],
+    link: "/wiki/mysticalautomation/blocks/infusion-altarnator"
+  },
+  {
+    title: "Enchanternator",
+    description: [
+      "Automatically craft Enchanter recipes using the Enchanternator."
+    ],
+    images: ["/assets/mysticalautomation/v1/blocks/enchanternator.png"],
+    link: "/wiki/mysticalautomation/blocks/enchanternator"
+  }
+]);
+</script>
