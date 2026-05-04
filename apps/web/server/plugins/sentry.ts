620768c6e664e41897a3466e69efe88372ab00ac
BlakeBr0 <luasmrks@gmail.com>
2026-05-03 23:13:30 -0500

    Ignore 'Page not found' errors in sentry

--- apps/web/server/plugins/sentry.ts
+++ apps/web/server/plugins/sentry.ts
@@ -3,6 +3,7 @@ import { sentryCloudflareNitroPlugin } from "@sentry/nuxt/module/plugins";
 export default defineNitroPlugin(
   sentryCloudflareNitroPlugin({
     dsn: "https://472cc4db584747768a1cb08281f678c4@o1148345.ingest.sentry.io/6219844",
-    tracesSampleRate: 0.1
+    tracesSampleRate: 0.1,
+    ignoreErrors: ["Page not found"]
   })
 );
